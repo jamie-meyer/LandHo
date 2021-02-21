88fe6585f128ab353d7c323736187f28755a8a6a
Jamie Meyer <jamesmey@usc.edu>
2021-02-20 18:52:53 -0800

    Fixed form submission

--- app/__init__.py
+++ app/__init__.py
@@ -26,15 +26,15 @@ def create_site():
     if request.method == 'GET':
         return render_template('creation_form.html')
     elif request.method == 'POST':
-        company = request.args.get('company')
-        address = request.args.get('address')
-        phone_number = request.args.get('phone_number')
-        hours = request.args.get('hours')
-        color = request.args.get('color')
-        if not company or not address or not phone_number or not hours or not color:
+        company = request.form['company']
+        address = request.form['address']
+        phone_number = request.form['phone_number']
+        hours = request.form['hours']
+        # color = request.form['color']
+        if not company or not address or not phone_number or not hours:
             return redirect(url_for('create_site'))
         return render_template('website_template.html', company=company, address=address, phone_number=phone_number,
-                               hours=hours, color=color)
+                               hours=hours)
 
 
 ###############################
